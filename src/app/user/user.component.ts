83d8fb3e1133921cc015b6948f3855d726425c71
ons-11 <ons.shili@esprit.tn>
2023-06-08 22:55:13 +0100

    chat

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,168 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Chat } from '../models/chat';
+import { User } from '../models/user';
+import { ChatService } from '../services/chat.service';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  chatId: any = 0;
+  registerForm: FormGroup;
+  loginForm: FormGroup;
+  successregister: boolean = false;
+  registermsg = "";
+  alert = "";
+  loginmsg = "";
+  alert2 = "";
+  successlogin: boolean = false;
+  public userObj: User = new User();
+  public alluser: any = [];
+  secondUsername = "";
+  chatObj: Chat = new Chat();
+  public chatData: any = [];
+  check = "";
+  loggedIn: boolean = false;
+  loggedOut: boolean = true;
+  chatbox: boolean = true;
+  username:any;
+
+  logout() {
+    this.loggedIn = false;
+    this.loggedOut = true;
+    sessionStorage.clear();
+    
+    this.router.navigateByUrl('');
+  }
+
+  constructor(private router: Router, private userService: UserService, private chatService: ChatService,private route:ActivatedRoute) {
+    this.registerForm = new FormGroup({
+      username: new FormControl("", [Validators.required])
+    });
+    this.loginForm = new FormGroup({
+      username: new FormControl("", [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      this.userService.getAll().subscribe((data) => {
+        // console.log(data);
+
+        this.alluser = data;
+        this.route.queryParams.subscribe(params=>{
+          this.username=params['username'];
+          console.log(this.username);
+          this.login();
+        })
+
+      })
+    }, 1000);
+
+  }
+
+  adduser() {
+    if (this.registerForm.valid) {
+      this.userObj.userName = this.registerForm.value.username;
+      this.userService.adduser(this.userObj).subscribe(
+        (data: any) => {
+          console.log(data);
+
+          this.successregister = true;
+          this.alert = "success";
+          this.registermsg = "Successsfully added";
+
+          this.registerForm.reset();
+
+        },
+        (error) => {
+          console.log(error.error);
+          if (error.status == 409) {
+            this.successregister = true;
+            this.alert = "danger";
+            this.registermsg = "Already registered"
+          } else {
+            this.successregister = true;
+            this.alert = "danger";
+            this.registermsg = "Error"
+          }
+
+        }
+      )
+    }
+
+  }
+
+  login() {
+    
+      this.userService.getUserByUsername(this.username).subscribe(
+        (data: any) => {
+          console.log(data);
+
+          this.successlogin = true;
+          this.alert2 = "success";
+          this.loginmsg = "Successsfully LoggedIn";
+
+          sessionStorage.setItem("username", this.username);
+          this.check = this.loginForm.value.username;
+          this.loginForm.reset();
+
+          this.loggedIn = true;
+          this.loggedOut = false;
+
+          // this.router.navigateByUrl('/chat');
+        },
+        (error) => {
+          console.log(error.error);
+          if (error.status == 404) {
+            this.successlogin = true;
+            this.alert2 = "danger";
+            this.loginmsg = "Not a registerd user"
+          } else {
+            this.successlogin = true;
+            this.alert2 = "danger";
+            this.loginmsg = "Error"
+          }
+
+        }
+      )
+    
+
+
+  }
+
+  goToChat(username: any) {
+    this.chatService.getChatByFirstUserNameAndSecondUserName(username, sessionStorage.getItem("username")).subscribe(
+      (data) => {
+        this.chatData = data;
+        this.chatId = this.chatData[0].chatId;
+        sessionStorage.setItem("chatId", this.chatId);
+        this.router.navigateByUrl('/chat');
+      },
+      (error) => {
+        if (error.status == 404) {
+          this.chatObj.firstUserName = sessionStorage.getItem("username");
+          this.chatObj.secondUserName = username;
+          this.chatService.createChatRoom(this.chatObj).subscribe(
+            (data) => {
+              this.chatData = data;
+              sessionStorage.setItem("chatId", this.chatData[0].chatId);
+              // this.router.navigateByUrl('/chat');
+              console.log("2")
+            })
+        } else {
+          // this.router.navigateByUrl('/chat');
+          console.log("3")
+        }
+      });
+
+  }
+
+}
